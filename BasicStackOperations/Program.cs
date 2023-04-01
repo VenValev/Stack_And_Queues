b53e4ba92b41eb44be4921b497399955b640fced
Vencislav <vencislav67@hotmail.com>
2023-04-01 12:15:06 +0300

    Add project files.

---
+++ BasicStackOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int s;
+            int x;
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
